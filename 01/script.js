119be9e7c1ef6a774e22eea855bc6a071da9beae
Jay <100322157+JayRenji@users.noreply.github.com>
2022-05-04 00:11:04 +0200

    Add files via upload
    Iteration#1

---
+++ 01/script.js
@@ -0,0 +1,12 @@
+console.log('Working');
+
+// **Iteración #1: Buscar el máximo**
+
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(num1 , num2) {
+    const suma = num1 + num2;
+    console.log('La suma de ' + num1 + ' y ' + num2 + ' es ' + suma);
+    
+  }
+  sum(2,2);
